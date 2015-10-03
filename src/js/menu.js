7e16eda548a9f60abbf2f56d0b9789fb3c70f84e
Leon Cheung <leonjcheung@gmail.com>
2015-10-03 07:04:55 -0700

    states are functional

---
+++ src/js/menu.js
@@ -0,0 +1,26 @@
+var menuState = {
+    create: function() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.world.setBounds(0, 0, 9999999, 600);
+        game.stage.backgroundColor = 0xffffff;
+
+        var startInstruction = game.add.text(80, game.world.height - 200,
+            "Press enter to start", 
+            { font: '25p Arial', fill: 0x000000 });
+
+        var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+        enterKey.onDown.addOnce(this.start, this);
+    },
+    preload: function() {
+        var menuLabel = game.add.text(80, 150, "Vinny's Veincation",
+            {font: '30px Courier', fill: 0x000000 });
+
+        game.load.spritesheet('cat', 'assets/home-cat.png', 64, 175);
+        game.load.image('platform', 'assets/platform.png');
+        game.load.image('bg', 'assets/bg.png');
+    },
+    start: function() {
+        console.log('sfff');
+        game.state.start('platform');
+    }
+};
