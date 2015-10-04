72ed52a13b46acbf5138bedb5282c337e35e450f
Leon Cheung <funarchitect2022@gmail.com>
2015-10-03 22:47:02 -0700

    Revert "framework rewrite"
    This reverts commit 7e8bed8991fe26687e9d0ee2ce2f59c7b7a02625.
    
    Conflicts:
    	src/assets/level1.json
    	src/assets/level1.tmx
    	src/js/platform.js

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,4 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
-game.state.add('startup', new startupState());
-game.state.add('menuState', new menuState());
-game.state.add('platform', new platformState());
-game.state.start("startup", true, false, "assets/levels.json");
+game.state.add('menu', menuState);
+game.state.add('platform', platformState);
+game.state.start('menu');
